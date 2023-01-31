e1e7255fe16aece93a5373028747f623faf33264
saish-res-phd <115307536+saish-res-phd@users.noreply.github.com>
2023-01-31 14:01:11 +0000

    Update node1_h1.py
    Script for generating and sending OpenFlow header fields on node h1

--- node1_h1.py
+++ node1_h1.py
@@ -1,19 +1,16 @@
-import ryu.ofproto.ofproto_v1_0 as ofp
-import socket
+import dpkt
 import time
+import socket
 
-# Create a Ryu OpenFlow header
+# Create an OpenFlow header
 def create_header():
-    msg = ryu.ofproto.ofproto_v1_0_parser.OFPHello(
-        datapath_id=0,
-        n_buffers=0,
-        n_tables=0,
-        capabilities=0,
-        actions=0
-    )
-    return msg
+    of = dpkt.openflow.OpenFlow()
+    of.version = 1
+    of.header_type = dpkt.openflow.OFPT_HELLO
+    of.xid = 0
+    return of.pack()
 
-# Create 20 Ryu OpenFlow headers
+# Create 20 OpenFlow headers
 headers = [create_header() for _ in range(20)]
 
 # Send headers to node h2
@@ -21,7 +18,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('10.0.0.2', 6653))
 start = time.time()
 for header in headers:
-    sock.send(header.buf)
+    sock.send(header)
 end = time.time()
 
 # Calculate elapsed time
