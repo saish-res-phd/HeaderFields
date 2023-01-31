2a5f66fc01a140794629b2f0d62a7d651122d5c8
saish-res-phd <115307536+saish-res-phd@users.noreply.github.com>
2023-01-31 16:24:01 +0000

    Update node1_h1.py

--- node1_h1.py
+++ node1_h1.py
@@ -1,26 +1,29 @@
 import socket
+import time
 
 IP = '10.0.0.2' # node h2 IP address
-PORT = 6653
+PORT = 6634
 HEADER_FIELDS = 20
 
 # create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect to node h2
-try:
-    s.connect((IP, PORT))
-except socket.error as e:
-    print("Error connecting to node h2:", e)
-    exit()
+# establish a connection to node h2
+connected = False
+start_time = time.time()
+while not connected:
+    try:
+        s.connect((IP, PORT))
+        connected = True
+    except socket.error:
+        if time.time() - start_time > 10:
+            print("Connection to node h2 failed")
+            break
+        time.sleep(1)
 
 # send the header fields
 for i in range(HEADER_FIELDS):
-    try:
-        s.send(str(i).encode())
-    except socket.error as e:
-        print("Error sending header field", i, ":", e)
-        exit()
+    s.send(str(i).encode())
 
-# close the connection
+# close the socket
 s.close()
