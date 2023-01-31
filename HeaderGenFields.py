3e32ec2bab018442452729093fc9e829ab73a693
saish-res-phd <115307536+saish-res-phd@users.noreply.github.com>
2023-01-31 14:36:25 +0000

    Update HeaderGenFields.py

--- HeaderGenFields.py
+++ HeaderGenFields.py
@@ -4,13 +4,13 @@ import struct
 
 # Define IP and port
 IP = '10.0.0.2' # IP of h2
-
+PORT= 6653
 
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to h2
-s.connect((IP))
+s.connect((IP,PORT))
 
 # Generate 20 OpenFlow header fields
 ofp_header_fields = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
